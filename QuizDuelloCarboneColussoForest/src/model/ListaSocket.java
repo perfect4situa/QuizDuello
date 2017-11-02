5d30106c9d1044854c13bda5c4d7eba6a27ae3c8
marco <marco@Jarvis>
2017-11-02 21:13:37 +0100

    Aggiunto metodo per chiudere tutti i socket

--- QuizDuelloCarboneColussoForest/src/model/ListaSocket.java
+++ QuizDuelloCarboneColussoForest/src/model/ListaSocket.java
@@ -1,22 +1,28 @@
 package model;
 
-import java.net.Socket;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class ListaSocket {
 	
-	private ArrayList<Socket> list;
+	private ArrayList<SocketQuiz> list;
 	
 	public ListaSocket() {
-		list = new ArrayList<Socket>();
+		list = new ArrayList<SocketQuiz>();
 	}
 
-	public ArrayList<Socket> getList() {
+	public ArrayList<SocketQuiz> getList() {
 		return list;
 	}
 
-	public void setList(ArrayList<Socket> list) {
+	public void setList(ArrayList<SocketQuiz> list) {
 		this.list = list;
+	}
+	
+	public void closeAll() throws IOException {
+		for(int i = 0; i < list.size(); i++) {
+			list.get(i).getSocket().close();
+		}
 	}
 
 }
