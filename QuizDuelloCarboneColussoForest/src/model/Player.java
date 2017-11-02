485b83f7ae808708cf6b0f6958376213a6df137c
marco <marco@Jarvis>
2017-11-02 21:17:40 +0100

    Aggiunta classe per gestire una lista giocatori perchè non penso che si possa o non so come prendere l'indirizzo ip di una connessione in entrata con server socket

---
+++ QuizDuelloCarboneColussoForest/src/model/Player.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Player {
+	
+	private String nickname;
+	
+	public Player(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+}
