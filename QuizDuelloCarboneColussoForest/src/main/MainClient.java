7e2142594d8c9c73e811d58b950d3df93f5942c8
marco <marco@Jarvis>
2017-11-05 12:54:50 +0100

    Aggiornato in base ai parametri del controller

--- QuizDuelloCarboneColussoForest/src/main/MainClient.java
+++ QuizDuelloCarboneColussoForest/src/main/MainClient.java
@@ -2,13 +2,15 @@ package main;
 
 import controller.ControllerClient;
 import view.FinestraConnessioneClient;
+import view.FinestraGiocoClient;
+import view.FinestraRisultati;
 
 public class MainClient {
 
 	public static void main(String[] args) {
 
 		@SuppressWarnings("unused")
-		ControllerClient ctrl = new ControllerClient(new FinestraConnessioneClient());
+		ControllerClient ctrl = new ControllerClient(new FinestraConnessioneClient(), new FinestraGiocoClient(), new FinestraRisultati());
 		
 	}
 
